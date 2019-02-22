4992576bc99545a7690000bd5b33cf7f340b4774
Joao Grassi <joaop.grassi@gmail.com>
2019-02-23 00:15:01 +0100

    Added a ASP.NET Core MVC app for demonstrating the authorization code with PKCE flow.

---
+++ src/MVCAppAuthorizationCodePKCE.NETCore/AppConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCAppAuthorizationCodePKCE.NETCore
+{
+    public class AppConstants
+    {
+        public const string TapkeyAuthorizationServerClient = "TapkeyAuthorizationServerClient";
+        public const string PKCECacheKey = "PKCE";
+        public const string AccessTokenCacheKey = "AccessToken";
+    }
+}
